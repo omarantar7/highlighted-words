0efb0cccb10210b5ba41a23b90b264497c6d6e02
omarantar7 <omarantar520@gmail.com>
2024-09-13 21:58:17 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
     return {
       searchTerm: "",
       paragraph:
-        "This is a simple Vue.js project. The words you search for will be highlighted here.",
+        "Your task is to create a search box where users can type in keywords or phrases, and your application returns results with the text being searched for highlighted. You can use any front-end framework you want (e.g. React, Angular, Vue.js, etc.).",
       count: 0,
     };
   },
